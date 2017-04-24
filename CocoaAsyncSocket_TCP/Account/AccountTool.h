ce1611f1e81e27bafa1b71b63089e46cced3d7ee
mengyaoyao <meng.yaoyao@jyall.com>
2017-04-24 16:14:52 +0800

    update

--- CocoaAsyncSocket_TCP/Account/AccountTool.h
+++ CocoaAsyncSocket_TCP/Account/AccountTool.h
@@ -5,6 +5,20 @@
 //  Created by 孟遥 on 2017/4/20.
 //  Copyright © 2017年 mengyao. All rights reserved.
 //
+/*
+ https://github.com/coderMyy/CocoaAsyncSocket_Demo  github地址 ,会持续更新关于即时通讯的细节 , 以及最终的UI代码
+ 
+ https://github.com/coderMyy/MYCoreTextLabel  图文混排 , 实现图片文字混排 , 可显示常规链接比如网址,@,话题等 , 可以自定义链接字,设置关键字高亮等功能 . 适用于微博,微信,IM聊天对话等场景 . 实现这些功能仅用了几百行代码，耦合性也较低
+ 
+ https://github.com/coderMyy/MYDropMenu  上拉下拉菜单，可随意自定义，随意修改大小，位置，各个项目通用
+ 
+ https://github.com/coderMyy/MYPhotoBrowser 简易版照片浏览器。功能主要有 ： 点击点放大缩小 ， 长按保存发送给好友操作 ， 带文本描述照片，从点击照片放大，当前浏览照片缩小等功能。功能逐渐完善增加中.
+ 
+ https://github.com/coderMyy/MYNavigationController  导航控制器的压缩 , 使得可以将导航范围缩小到指定区域 , 实现页面中的页面效果 . 适用于路径选择,文件选择等
+ 
+ 如果有好的建议或者意见 ,欢迎博客或者QQ指出 , 您的支持是对贡献代码最大的鼓励,谢谢. 求STAR ..😊😊😊
+ */
+
 
 #import <Foundation/Foundation.h>
 #import "Account.h"
